653f3a26db27d069c5cbdbc728ba729ec815abbf
Kiruthika.Srinivasan <Kiruthika.Srinivasan@mnsukcate.adrootcate.marksandspencercate.com>
2017-04-26 12:20:38 +0100

    updated tc-04

--- src/test/java/com/jda/wms/hooks/Hooks.java
+++ src/test/java/com/jda/wms/hooks/Hooks.java
@@ -3,15 +3,19 @@ package com.jda.wms.hooks;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Key;
+import org.sikuli.script.Screen;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.google.inject.Inject;
-
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 
 public class Hooks {
-
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private final WebDriver webDriver;
+	Screen screen = new Screen();
 
 	@Inject
 	public Hooks(WebDriver webDriver) {
@@ -30,5 +34,24 @@ public class Hooks {
 			webDriver.close();
 			webDriver.quit();
 		}
+	}
+	
+	@After()
+	public void logoutPutty() throws FindFailed, InterruptedException{
+		while (screen.exists("/images/Putty/3Logout.png") == null){
+			screen.type(Key.F12);
+		}
+		screen.type("3");
+		Thread.sleep(1000);
+		screen.type(Key.ENTER);
+		Thread.sleep(2000);
+		
+		screen.wait("images/Putty/PuttyClose.png", 20);
+		screen.click("images/Putty/PuttyClose.png", 25);
+		Thread.sleep(1000);
+		
+		screen.wait("images/Putty/PuttyCloseOK.png", 20);
+		screen.click("images/Putty/PuttyCloseOK.png", 25);
+		Thread.sleep(1000);
 	}
 }
--- src/test/java/com/jda/wms/utils/Utilities.java
+++ src/test/java/com/jda/wms/utils/Utilities.java
@@ -8,13 +8,14 @@ public class Utilities {
 		return (int) Double.parseDouble(stringToConvert);
 	}
 
-	public String getTenDigitRandomNumber() {
+	public int getTenDigitRandomNumber() {
 		Random r = new Random();
-		int ran1 =  (int) Math.round(Math.random()*1000000000); 
-		System.out.println(ran1);
-		int ran2 = (int) Math.round(Math.random()*10);
-		System.out.println(ran2);
-		String uniqueID = String.valueOf(ran1)+String.valueOf(ran2);
-		return uniqueID;
+		// int ran1 = (int) Math.round(Math.random()*1000000000);
+		// System.out.println(ran1);
+		// int ran2 = (int) Math.round(Math.random()*10);
+		// System.out.println(ran2);
+		// String uniqueID = String.valueOf(ran1)+String.valueOf(ran2);
+		return r.nextInt(100000000) + 999999;
+		// return uniqueID;
 	}
 }
