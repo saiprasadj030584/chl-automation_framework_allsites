1340c7b4eebc8a20e2f89046549c067a16413513
Kiruthika.Srinivasan <Kiruthika.Srinivasan@mnsukcate.adrootcate.marksandspencercate.com>
2017-07-04 07:52:52 +0100

    updating tag in all test

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -10,7 +10,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
-		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { "@wip_dock"})
+		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { "@complete_po"})
 public class AllTest {
 }
 
