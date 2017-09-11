63c3abe136cf41230283b1b45baf94c507eab953
Kiruthika.Srinivasan <Kiruthika.Srinivasan@mnsukcate.adrootcate.marksandspencercate.com>
2017-09-11 10:54:20 +0100

    updating code

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -7,8 +7,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(monochrome = true, features = "src/test" + "/resources/features", glue = { "com.jda.wms" }, plugin = {
-		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { "@jenkins" })
+@CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
+		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { "@jenkins_check" })
 
 public class AllTest {
 }
