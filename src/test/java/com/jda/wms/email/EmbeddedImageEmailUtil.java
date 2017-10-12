9005abb2f3887d70c77ee413db34fa3c31ac0e4c
Kiruthika.Srinivasan <Kiruthika.Srinivasan@mnsukcate.adrootcate.marksandspencercate.com>
2017-10-12 10:31:08 +0100

    updated cucumber report code

--- src/test/java/com/jda/wms/email/EmbeddedImageEmailUtil.java
+++ src/test/java/com/jda/wms/email/EmbeddedImageEmailUtil.java
@@ -122,12 +122,17 @@ public class EmbeddedImageEmailUtil {
 		}
 
 //		String filePath = envVar+"/files";
+		File reportPath = new File(configuration.getStringProperty("cucumber-zip-path"));
+		if (reportPath.listFiles().length!=0){
 		String filePath = configuration.getStringProperty("cucumber-zip-path");
 		String fileName = "Cucumber_Report.zip";
 		MimeBodyPart attachmentBodyPart = new MimeBodyPart();
 		attachmentBodyPart.attachFile(new File(filePath + "/" + fileName));
 		multipart.addBodyPart(attachmentBodyPart);
-		
+		}
+		else{
+			System.out.println("Cucumber report not found and so not attached in Email");
+		}
 		msg.setContent(multipart);
 
 		Transport.send(msg);
