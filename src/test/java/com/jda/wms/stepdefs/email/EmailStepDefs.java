cdf644a96f0032e3c3fb7a28a6f9e7e94a4d8c1c
Kiruthika.Srinivasan <Kiruthika.Srinivasan@mnsukcate.adrootcate.marksandspencercate.com>
2017-10-24 08:31:43 +0100

    updating code

--- src/test/java/com/jda/wms/stepdefs/email/EmailStepDefs.java
+++ src/test/java/com/jda/wms/stepdefs/email/EmailStepDefs.java
@@ -99,11 +99,6 @@ public class EmailStepDefs {
 	
 	@Then("^I update the cucumber reports for the js files$")
 	public void i_update_the_cucumber_reports_for_the_js_files() throws Throwable { 
-		//Triggering the Copy_files Bat file 
-		System.out.println("cmd /c \"" + envVar + "\\Copy_Files_GMAdmin.lnk\"");
-		Thread.sleep(2000);
-		Process p1 = Runtime.getRuntime().exec("cmd /c \"" + envVar + "\\Copy_Files_GMAdmin.lnk\"");
-		
 		//Replacing the JS code lines in HTML files to cloud URLs
 		String replaceJqueryURL = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
 		String replaceTableSorterURL = "https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.29.0/js/jquery.tablesorter.min.js";
