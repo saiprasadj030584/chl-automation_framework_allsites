160c59b5983e8640d4331bb51dcbf83ff7b8fb54
Kiruthika.Srinivasan <Kiruthika.Srinivasan@mnsukcate.adrootcate.marksandspencercate.com>
2017-10-25 17:08:32 +0100

    updated changes to run maven_check scripts

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
-		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = {"@maven_check_1"})
+		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = {"@boxed_pre_receiving_fsv_po_validate_whether_booking_details_can_be_captured_trailer_type_information"})
 
 public class AllTest {
 }
