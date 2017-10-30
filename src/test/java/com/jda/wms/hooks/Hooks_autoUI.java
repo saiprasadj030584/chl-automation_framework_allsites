58ee88e2b05866bd2f94ce1d0f54fe7bbfa29289
Santhaseelan.Shanmugam <Santhaseelan.Shanmug@mnsukdev.adrootdev.marksandspencerdev.com>
2017-10-30 12:51:48 +0000

    updayted codes

--- src/test/java/com/jda/wms/hooks/Hooks_autoUI.java
+++ src/test/java/com/jda/wms/hooks/Hooks_autoUI.java
@@ -148,14 +148,14 @@ public class Hooks_autoUI {
 //		}
 	}
 	
-	@After
-	public void killIE(Scenario scenario) throws IOException {
-		if (webDriver != null) {
-			webDriver.close();
-			Process p = Runtime.getRuntime().exec("cmd /c " + envVar + "\\bin\\IEKill.bat");
-			// webDriver.quit();
-		}
-	}
+//	@After
+//	public void killIE(Scenario scenario) throws IOException {
+//		if (webDriver != null) {
+//			webDriver.close();
+//			Process p = Runtime.getRuntime().exec("cmd /c " + envVar + "\\bin\\IEKill.bat");
+//			// webDriver.quit();
+//		}
+//	}
 
 	public void insertDetails(String testName) {
 
