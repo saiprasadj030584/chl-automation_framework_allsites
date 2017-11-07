40d7a417596def8c09890d4f00b8ed8ef5c120cf
Indira.Sankaran <Indira.Sankaran@mnsukcate.adrootcate.marksandspencercate.com>
2017-11-07 14:14:05 +0000

    adding codes

---
+++ src/test/java/com/jda/wms/pages/gm/SchedulerProgramPage.java
@@ -0,0 +1,25 @@
+package com.jda.wms.pages.gm;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Match;
+import com.google.inject.Inject;
+import org.sikuli.script.Screen;
+public class SchedulerProgramPage {
+	private JDAFooter jDAFooter;
+	Screen screen = new Screen();
+	int timeoutInSec = 20;
+	
+	@Inject
+	public SchedulerProgramPage(JDAFooter jDAFooter) {
+		this.jDAFooter = jDAFooter;
+	}
+	public void enterProgramName() throws InterruptedException {
+		screen.type("CLUSTERURNSP");
+		Thread.sleep(1000);
+		
+	}
+	public void clickRunNowButton() throws FindFailed, InterruptedException {
+		Match run = screen.find("images/schedulerProgram/runButton.png");
+		screen.click(run.getCenter().offset(70, 0));
+		Thread.sleep(2000);
+	}
+}
