dc20edd55b44936bc95729b3d89967fb06807f58
Fathima.Jaseela <Fathima.Jaseela@mnsukcate.adrootcate.marksandspencercate.com>
2017-11-08 18:08:07 +0000

    updated codes

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 
-		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { "@goh_stock_adjustment_inventory_update_unlock_the_stock_from_lock"})
+		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { "@flatpack_stock_adjustment_stock_adjustment_reason_code_do_stock_adjustment_by_selecting_reason_code_as_incomplete_"})
 
 public class AllTest {
 }	
