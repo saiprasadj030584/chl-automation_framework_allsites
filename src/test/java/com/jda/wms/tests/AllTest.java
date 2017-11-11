78adca95fd29f8ccd0ac10ff5af405ff67175eb2
Fathima.Jaseela <Fathima.Jaseela@mnsukcate.adrootcate.marksandspencercate.com>
2017-11-11 07:36:46 +0000

    updated tags

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 
-		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = {"@hanging_receiving_direct_po_validate_receiving_process_with_qafts_lock_code"})
+		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = {"@jenkinsfsvpr"})
 
 public class AllTest {
 }	
