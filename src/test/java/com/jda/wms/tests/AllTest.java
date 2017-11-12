d063a24011bede1e96be3de4ecb37ddeecdbbbb9
Fathima.Jaseela <Fathima.Jaseela@mnsukcate.adrootcate.marksandspencercate.com>
2017-11-12 12:58:20 +0000

    updating email

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 
-		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = {"@hanging_pre_receiving_direct_po_validate_the_product_attributes_are_available_for_the_receiving_process"})
+		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = {"@Email"})
 
 public class AllTest {
 }	
