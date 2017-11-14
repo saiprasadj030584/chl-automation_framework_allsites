d481664a959c245d5e91a193ae898518930d437d
Fathima.Jaseela <Fathima.Jaseela@mnsukcate.adrootcate.marksandspencercate.com>
2017-11-14 13:30:40 +0000

    updated tags

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 
-		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = {"@hanging_receiving_direct_po_validate_receipting_process_without_lock_code"})
+		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = {"@hanging_receiving_direct_po_validate_not_received_ASN"})
 
 public class AllTest {
 }	
