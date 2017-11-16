21b6bd53330234cf7278da489528bbc28d081501
Fathima.Jaseela <Fathima.Jaseela@mnsukcate.adrootcate.marksandspencercate.com>
2017-11-16 14:23:31 +0000

    tags

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 
-		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = {"@goh_outbound_order_till_despatch_store_order_short_store_order_full_cancel"})
+		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = {"@boxed_receiving_direct_po_receiving_with_lock_code_qafts"})
 
 public class AllTest {
 }	
