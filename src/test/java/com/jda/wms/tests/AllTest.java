2e42b947308f13183a7ba1443090f59fe9e90c66
Fathima.Jaseela <Fathima.Jaseela@mnsukcate.adrootcate.marksandspencercate.com>
2017-12-11 06:37:50 +0000

    tags

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = {
-				"@unique_boxed_allocation_e_com_validate_whether_all_the_stocks_are_allocated_allocation_rules " })
+				"@unique_hanging_order_management_retail_order_verify_order_status_updated_for_each_transaction_in_order_management_screen_and_order_header " })
 
 public class AllTest {
 
