f53e083292f2cf181ce4cbd9e22f03aa64cd63d0
gomathi.venkatesan <gomathi.venkatesan@MSHSRMNSUKC0518.mnsukcate.adrootcate.marksandspencercate.com>
2018-11-29 06:36:45 +0000

    check

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -15,16 +15,7 @@ import cucumber.api.junit.Cucumber;
 
 
 
-
-				+ "@TC01_Batch_and_Expiry_Date_Check" 
-
-
-				 
-
-
-
-
-			
+				+ "@TC06_Validate_Compliance_check_commodity_code_is_null_or_invalid" 
 
 
 })
