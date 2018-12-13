79afc03f10839f33b9fb3f2e2dd437bee9233a99
sagorika.samanta <sagorika.samanta@MSHSRMNSUKC0270>
2018-12-13 12:12:44 +0000

    30

--- src/test/java/com/jda/wms/stepdefs/Exit/JDAExitputtyfunctionsStepDef.java
+++ src/test/java/com/jda/wms/stepdefs/Exit/JDAExitputtyfunctionsStepDef.java
@@ -508,14 +508,14 @@ public class JDAExitputtyfunctionsStepDef {
 		storeTrackingOrderPickingPage.selectsortation();
 		
 	}
-	@Given("I enter URN")
-	public void I_enter_URN() throws Throwable
-	{
-		String palletIDforUPI = context.getpalletIDforUPI();
-		System.out.println("palletID "+palletIDforUPI);
-		purchaseOrderReceivingPage.EnterPalletID(palletIDforUPI);
-		puttyFunctionsPage.pressEnter();
-	}
+//	@Given("I enter URN")
+//	public void I_enter_URN() throws Throwable
+//	{
+//		String palletIDforUPI = context.getpalletIDforUPI();
+//		System.out.println("palletID "+palletIDforUPI);
+//		purchaseOrderReceivingPage.EnterPalletID(palletIDforUPI);
+//		puttyFunctionsPage.pressEnter();
+//	}
 	@Given("^I enter To Pallet for two urn$")
 	public void I_enter_To_Pallet_for_two_urn() throws Throwable {
 		String ToPallet = null;
