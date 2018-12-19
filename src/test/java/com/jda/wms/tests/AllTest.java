cb8a2755dfb333718f4638d62b6155107387c219
priyanka.selvaraj <priyanka.selvaraj@MSHSRMNSUKC0518.mnsukcate.adrootcate.marksandspencercate.com>
2018-12-19 04:26:11 +0000

    resolved

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -12,7 +12,6 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { ""
-			+ "@TC07_Validate_the_auto_picking_process_for_the_Cross_dock_FSV_order" 
 
 })
 public class AllTest {
