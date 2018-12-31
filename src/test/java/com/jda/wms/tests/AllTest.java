1d6aee2eff271e87f04b86f74e1868b2bebd3980
gomathi.venkatesan <gomathi.venkatesan@MSHSRMNSUKC0518.mnsukcate.adrootcate.marksandspencercate.com>
2018-12-31 07:16:29 +0000

    commit

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -12,7 +12,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { ""
-			+ "@TC39_Validate_container_report" 
+			+ "@TC02_Validate_the_MNS_Dangerous_Goods_DGN_Report" 
 
 })
 public class AllTest {
