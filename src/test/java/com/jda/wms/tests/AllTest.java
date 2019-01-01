c227d39f4aa5bab936314eb5968d5d088b143913
gomathi.venkatesan <gomathi.venkatesan@MSHSRMNSUKC0518.mnsukcate.adrootcate.marksandspencercate.com>
2019-01-01 11:41:41 +0000

    commited 1

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -12,7 +12,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { ""
-			+ "@TC18_Validate_the_M_N_S_URN_Audit_Trail_Report" 
+			+ "@TC074_Load_systemic_reports_revised_on_amended_in_Consignment" 
 
 })
 public class AllTest {
