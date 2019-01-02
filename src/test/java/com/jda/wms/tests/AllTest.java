3b465ec6265343cf223242a66104b4fa19f6e0cd
gomathi.venkatesan <gomathi.venkatesan@MSHSRMNSUKC0518.mnsukcate.adrootcate.marksandspencercate.com>
2019-01-02 11:48:13 +0000

    merge commit

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -12,7 +12,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { ""
-			+ "@TC27_Negative_Path_Incorrect_pallet_consignment_linking" 
+			+ "@TC71_access_for_CIR_Report_generation" 
 
 })
 public class AllTest {
--- src/test/resources/features/Exit/Sprint03_Picking.feature
+++ src/test/resources/features/Exit/Sprint03_Picking.feature
@@ -246,6 +246,7 @@ Feature: Orders_Picking
  		Examples: 
       | SKU                |
       | 000000000021071852 |
+      
  @Sortation @TC28_Happy_Path_Validate_consignment_id_format
   Scenario: Happy_Path_Validate consignment id format
     Given Login to JDA Dispatcher web screen
