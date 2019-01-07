c6afd110864cc138d77fd6dae1a14d8744bddf6b
sagorika.samanta <sagorika.samanta@MSHSRMNSUKC0270>
2019-01-07 13:09:44 +0000

    done

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -12,7 +12,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { ""
-			+ "@TC34_Negative_Path_container_and_consignment" 
+			+ "@TC041_Negative_path_Incorrect_Pallet_for_consignment_trailer_linking" 
 
 })
 public class AllTest {
