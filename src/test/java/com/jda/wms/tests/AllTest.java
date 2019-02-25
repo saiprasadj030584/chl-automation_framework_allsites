faa8545e10037e009c310aca6ae97d92f1e25bf2
Gomathi.Venkatesan <Gomathi.Venkatesan@MSHSRMNSUKC0270>
2019-02-25 06:47:58 +0000

    commit and push1

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -12,8 +12,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { ""
-			+ "@TC03_Compliance_Check"
-			+ ""
+			+ "@TC29_Validate_confirm_shipment"	
+		     + ""
 			+ "" 
 
 })
