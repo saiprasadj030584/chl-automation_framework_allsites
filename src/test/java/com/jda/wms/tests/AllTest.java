e253f7bc278ca31b93bd165bf0a8d5792a70032e
bhubaneswarsethi <bhubaneswar.sethi@marks-and-spencer.com>
2019-03-14 07:44:20 +0000

    commit1

--- src/test/java/com/jda/wms/tests/AllTest.java
+++ src/test/java/com/jda/wms/tests/AllTest.java
@@ -13,7 +13,8 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(monochrome = true, features = "src/test/resources/features", glue = { "com.jda.wms" }, plugin = {
 		"pretty", "json:target/cucumber-reports/cucumber.json" }, tags = { ""
 			+ ""
-			+ "@Sprint06"
+			+ "@TC021_Validate_Reversion_relocate_storage_location"
+			+ ""
 			+ ""	
 		     + ""
 			+ "" 
